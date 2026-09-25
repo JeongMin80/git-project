0a175ae631a50c3abd536a2ef01d17d547f9f32b
JeongMin80 <leejongmin2838@gmail.com>
2026-09-25 20:02:22 +0900

    Add multiply and divide functions

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-function add() {
+function multiply() {
     const num1 = Number(document.getElementById("num1").value);
     const num2 = Number(document.getElementById("num2").value);
 
-    const result = num1 + num2;
+    const result = num1 * num2;
 
     document.getElementById("result").textContent = result;
 }
 
-function subtract() {
+function divide() {
     const num1 = Number(document.getElementById("num1").value);
     const num2 = Number(document.getElementById("num2").value);
 
-    const result = num1 - num2;
+    if (num2 === 0) {
+        document.getElementById("result").textContent = "0으로 나눌 수 없습니다.";
+        return;
+    }
+
+    const result = num1 / num2;
 
     document.getElementById("result").textContent = result;
 }
